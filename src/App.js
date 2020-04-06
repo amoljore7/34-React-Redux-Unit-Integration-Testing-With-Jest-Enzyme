b4338be3d825ed6d14f12c7f958e4f8e0954b86f
amoljore7 <amoljore7@gmail.com>
2020-04-06 12:49:45 +0530

    Added styling logo in component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from './component/header/index';
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello
+      <div className="App">
+        <Header />
       </div>
     )
   }
